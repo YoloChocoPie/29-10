a3f7e153b8f0abe9983a831dd9851e1677872ecd
YoloChocoPie <chocopieyolo@gmail.com>
2019-10-29 14:49:15 +0700

    vongtron

---
+++ baitap/baitap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+      Vongtron c1 = new Vongtron();
+
+      Console.WriteLine("The circle has radius of " + c1.getRadius() + " and area of " + c1.getArea());  
+
+      Vongtron c2 = new Vongtron(2.0);
+
+      Console.WriteLine("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea());
+
+     //Vongtron c3 = new Vongtron(3, "Black");
+     // Console.WriteLine(" Co ban kinh " + c3.getRadius() + " va mau " + c3.getRadius(), c3.getRadius());
+     //     c3.
+
+        }
+    }
+}
---
+++ baitap/baitap/Vongtron.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitap
+{
+    public class Vongtron
+    {
+        
+            private double radius;
+            private String _Color;
+
+
+            public Vongtron()
+            {
+                radius = 1.0;
+                _Color = "red";
+            }
+
+
+            public Vongtron(double r)
+            {
+                radius = r;
+                _Color = "red";
+            }
+
+
+            public double getRadius()
+            {
+                return radius;
+            }
+
+
+            public double getArea()
+            {
+                return radius * radius * Math.PI;
+            }
+          //  public string _Color()
+          //  { 
+             //   get;
+           //     {
+            //        return this._Color;
+           //     }
+            //    Set;
+           //     {
+           //         this._Color = Value;
+             //   }
+           // }
+            
+            public Vongtron(double r, String c)
+            {
+                radius = r;
+                _Color = c;
+            }
+            
+            public String GetColor()
+            {
+                return _Color;
+            }
+        public void SetRadius(double newRadius)
+            {
+                radius = newRadius;
+            }
+            
+        }
+    }
+
